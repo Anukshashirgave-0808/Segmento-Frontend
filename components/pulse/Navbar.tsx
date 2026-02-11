09866e43b24fa2ebce0facea18b1cf2326955fd0
SHAFI <iamshafisk@gmail.com>
2026-02-11 19:09:07 +0530

    fixed 'go back to segmento' button on the navbar for mobiles

--- components/pulse/Navbar.tsx
+++ components/pulse/Navbar.tsx
@@ -534,6 +534,17 @@ export default function PulseNavbar({ onSubscribeClick }: { onSubscribeClick?: (
                                     <Mail className="h-4 w-4 mr-2" />
                                     Subscribe
                                 </Button>
+
+                                {/* Get back to Segmento - Mobile Version */}
+                                <Button
+                                    asChild
+                                    className="bg-gradient-to-r from-indigo-600 to-purple-600 hover:from-indigo-700 hover:to-purple-700 justify-start"
+                                    onClick={() => setIsMenuOpen(false)}
+                                >
+                                    <Link href="/">
+                                        Get back to Segmento
+                                    </Link>
+                                </Button>
                             </div>
                         </nav>
                     </div>
